826f78e93409471eaf5c6893fc0ea94cf3035c3d
江先礼 <997204035@qq.com>
2016-10-25 14:40:47 +0800

    fix bug

--- app/Model/User.php
+++ app/Model/User.php
@@ -10,6 +10,13 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
